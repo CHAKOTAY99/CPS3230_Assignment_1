dea009945189a539f374adfe20ebc72a7f79bc0a
Chakotay Incorvaja <chakotay.incorvaja@gmail.com>
2019-11-05 00:14:37 +0100

    Task 2: DefaultCurrencyServer Completed

--- src/test/java/edu/uom/currencymanager/currencyServer/DefaultCurrencyServerTest.java
+++ src/test/java/edu/uom/currencymanager/currencyServer/DefaultCurrencyServerTest.java
@@ -1,4 +1,19 @@
 package edu.uom.currencymanager.currencyServer;
 
+import edu.uom.currencymanager.currencyserver.DefaultCurrencyServer;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
 public class DefaultCurrencyServerTest {
+
+    @Test
+    public void test_getExchangeRateResult(){
+        // Setup
+        DefaultCurrencyServer defaultCurrencyServer = new DefaultCurrencyServer();
+        String sourceCurrency = "RMB", destinationCurrency = "EGP";
+        // Exercise
+        double result = defaultCurrencyServer.getExchangeRate(sourceCurrency, destinationCurrency);
+        assertTrue(result < 1.5 && result > 0);
+    }
 }
