c9f1adf117d2f82c6a967f4d77529de7bf3e4dd4
Chakotay Incorvaja <chakotay.incorvaja@gmail.com>
2019-11-04 22:50:55 +0100

    Task 2: ExchangeRateTest done

---
+++ src/test/java/edu/uom/currencymanager/currencies/ExchangeRateTest.java
@@ -0,0 +1,38 @@
+package edu.uom.currencymanager.currencies;
+
+import org.junit.After;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExchangeRateTest {
+
+    @Test
+    public void constructorTest(){
+        // Setup
+        Currency sourceCurrency = new Currency("EGP", "Egyptian Pound", false);
+        Currency destinationCurrency = new Currency("RMB", "Rembini", true);
+        double rate = 1.0;
+        long timeLastChecked = System.currentTimeMillis();
+        // Exercise
+        ExchangeRate exchangeRate = new ExchangeRate(sourceCurrency, destinationCurrency, rate);
+        // Verify
+        assertTrue(Math.abs(exchangeRate.rate-rate) <= 10);
+        assertTrue(Math.abs(exchangeRate.timeLastChecked-timeLastChecked)<=10);
+        assertEquals(exchangeRate.sourceCurrency, sourceCurrency);
+        assertEquals(exchangeRate.destinationCurrency, destinationCurrency);
+    }
+
+
+    @Test
+    public void testToString_returnValid(){
+        // Setup
+        Currency sourceCurrency = new Currency("EGP", "Egyptian Pound", false);
+        Currency destinationCurrency = new Currency("RMB", "Rembini", true);
+        double rate = 1;
+        // Exercise
+        ExchangeRate exchangeRate = new ExchangeRate(sourceCurrency, destinationCurrency, rate);
+        String result = exchangeRate.toString();
+        assertEquals("EGP 1 = RMB 1.00", result);
+    }
+}
